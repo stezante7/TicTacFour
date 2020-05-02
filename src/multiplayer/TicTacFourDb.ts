bc9d29abdd8de59868232d530f928e798cf03020
Stefano Zante <stezante7@gmail.com>
2020-05-02 16:04:57 +0100

    Added db config

--- src/multiplayer/TicTacFourDb.ts
+++ src/multiplayer/TicTacFourDb.ts
@@ -1,19 +1,8 @@
 import * as firebase from "firebase";
-
-// Your web app's Firebase configuration
-var firebaseConfig = {
-  apiKey: "<api-key>",
-  authDomain: "<auth-domain>",
-  databaseURL: "<database-url>",
-  projectId: "<project-id>",
-  storageBucket: "<storage-bucket>",
-  messagingSenderId: "<messaging-sender-id>",
-  appId: "<app-id>",
-  measurementId: "<measurement-id>",
-};
+import config from "./db.config";
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(config);
 firebase.analytics();
 
 const db = firebase.firestore();
---
+++ src/multiplayer/db.config.ts
@@ -0,0 +1,12 @@
+const dbConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
+};
+
+export default dbConfig;
